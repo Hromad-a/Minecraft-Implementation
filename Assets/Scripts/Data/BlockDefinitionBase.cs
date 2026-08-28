71b02fb88ae5e85662178a53d70cd1e03e16e6d3
Martin Hromádka <martinhromadka7@gmail.com>
2026-08-28 11:58:49 +0200

    Blocks data

--- Assets/Scripts/Data/BlockDefinitionBase.cs
+++ Assets/Scripts/Data/BlockDefinitionBase.cs
@@ -1,3 +1,4 @@
+using NaughtyAttributes;
 using UnityEngine;
 
 public abstract class BlockDefinitionBase : ScriptableObject
@@ -5,8 +6,7 @@ public abstract class BlockDefinitionBase : ScriptableObject
     [SerializeField, Min(0)] private int id;
     [SerializeField] private Material material;
     [SerializeField] private float mineDuration = 1f;
-    [SerializeField, Min(0)] private int minHeight;
-    [SerializeField, Min(0)] private int maxHeight;
+    [SerializeField, MinMaxSlider(0f, 1f)] private Vector2 heightRange;
 
     public float MineDuration => mineDuration;
     public bool IsUnbreakable => mineDuration < 0f;
